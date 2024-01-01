56adbabe8be1af7ef1b934d76f791f54803672f8
Mor Marzan <mormarzan@gmail.com>
2024-01-01 14:08:42 +0200

    Add cloudinary imgs

--- src/cmps/ToyPreview.jsx
+++ src/cmps/ToyPreview.jsx
@@ -5,6 +5,7 @@ import { utilService } from '../services/util.service'
 export function ToyPreview({ toy }) {
     const dynClass = !toy.inStock ? 'out-of-stock' : ''
 
+    const imgNames = ["train_lk4zzl", "puzzle-box_hw7q1t", "rubber-duck_gao8pi"]
     const { name, price, inStock } = toy
 
     return (
@@ -12,8 +13,10 @@ export function ToyPreview({ toy }) {
             <h3>{utilService.capitalizeFirstLetter(name)}</h3>
             <h4>{price}$</h4>
             <h4>{inStock ? 'Avialable' : 'Out of stock'}</h4>
-            <img src={dollPng} />
+            {/* <img src={dollPng} /> */}
+            <img src={`https://res.cloudinary.com/daydgnssy/image/upload/v1704110027/${imgNames[utilService.getRandomIntInclusive(0, imgNames.length - 1)]}.png`} />
 
         </article>
     )
 }
+
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 
-import legoPng from '../assets/img/lego.jpg'
-import dollPng from '../assets/img/bear.png'
+// import dollPng from '../assets/img/bear.png'
 
 export function HomePage() {
 
     return (
         <section className='page home-page'>
             <h2>Welcome to our amazing store!</h2>
-            <img src={dollPng} />
+            {/* <img src={dollPng} /> */}
+            <img src="https://res.cloudinary.com/daydgnssy/image/upload/v1704109682/ballons_cby3pb.png" />
         </section >
     )
 }
